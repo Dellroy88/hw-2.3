4c3a681cafba8079a14af54b33daa7bb1ac62d53
Семья Ким <dell200288@yandex.ru>
2022-09-19 19:10:14 +0300

    HW-2.3 done

--- src/ServiceStation.java
+++ src/ServiceStation.java
@@ -1,16 +1,13 @@
 public class ServiceStation {
 
     public void check(Car car) {
-        if (car != null) {
             System.out.println("Обслуживаем " + car.getModelName());
             for (int i = 0; i < car.getWheelsCount(); i++) {
                 car.updateTyre();
             }
             car.checkEngine();
         }
-    }
     public void check(Truck truck) {
-        if (truck != null) {
             System.out.println("Обслуживаем " + truck.getModelName());
             for (int i = 0; i < truck.getWheelsCount(); i++) {
                 truck.updateTyre();
@@ -18,13 +15,10 @@ public class ServiceStation {
             truck.checkEngine();
             truck.checkTrailer();
         }
-    }
     public void check(Bicycle bicycle){
-        if (bicycle != null) {
             System.out.println("Обслуживаем " + bicycle.getModelName());
             for (int i = 0; i < bicycle.getWheelsCount(); i++) {
                 bicycle.updateTyre();
             }
-        }
     }
 }
